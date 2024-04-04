9325b688675c02058cc891473c414d8d37973c92
Juan <Jinbyos@Gmail.com>
2024-04-04 00:04:17 -0300

    commit inicial

--- Backend/API/Entities/Product.cs
+++ Backend/API/Entities/Product.cs
@@ -6,6 +6,7 @@
         public string Description { get; set; }
         public decimal Price { get; set; }
         public string Name { get; set; }
+        public bool Available { get; set; }
 
 
         // Navigation property
