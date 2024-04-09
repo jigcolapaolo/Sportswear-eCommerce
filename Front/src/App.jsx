d5e74c3f98b8d9e53a10fa52af25a7967794eefd
Nicolas <nicolas.esc100@gmail.com>
2024-04-09 19:44:39 -0300

    Navbar de miguel

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -1,15 +1,17 @@
-import Articulo from "./components/Articulo/Articulo"
-import ArticuloFotos from "./components/Articulo/ArticuloFotos"
-import { CategoryGrid } from "./components/CategoryGrid/CategoryGrid"
+import Articulo from "./components/Articulo/Articulo";
+import ArticuloFotos from "./components/Articulo/ArticuloFotos";
+import { CategoryGrid } from "./components/CategoryGrid/CategoryGrid";
+import NavBar from "./components/NavBar/NavBar";
 
 function App() {
   return (
-    <main className="bg-[#212121]">
-    <CategoryGrid />
-    <Articulo/>
-    <ArticuloFotos/>
+    <main className='bg-[#212121]'>
+      <NavBar />
+      <CategoryGrid />
+      <Articulo />
+      <ArticuloFotos />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
