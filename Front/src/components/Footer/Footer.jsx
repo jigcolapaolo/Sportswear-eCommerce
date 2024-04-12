72abb7712b1e150f92c2c86255c0798930024ad5
Miguel Salazar <yobebe@hotmail.com.ar>
2024-04-12 15:20:23 -0300

    [200~Muchachos ya termine el footer, dentro de la carpeta public, dentro de la carpeta imagenes van a poder ver dos carpetas llamadas iconos y iconosDooter que yo las cree para poder poner las imagenes de la navbr como del footer~

--- Front/src/components/Footer/Footer.jsx
+++ Front/src/components/Footer/Footer.jsx
@@ -1,11 +1,39 @@
-
-
 const Footer = () => {
   return (
-    <div className="w-{1440px} h-{255px} t-{827px} bg-{#EFEFDC}"> 
-        <p> Sobre nosotros </p>
-    </div>
-  )
-}
+    <footer className="bg-gray-200 w-full h-255 border-t-8 mt-827 flex justify-between items-center px-10">
+      <div className="flex flex-col">
+        <p className="font-roboto font-normal text-base leading-28.13 text-gray-900">
+          Sobre nosotros
+        </p>
+        <p className="font-roboto font-normal text-base leading-28.13 text-gray-900">
+          Preguntas frecuentes
+        </p>
+        <p className="font-roboto font-normal text-base leading-28.13 text-gray-900">
+          Medios de pago
+        </p>
+        <p className="font-roboto font-normal text-base leading-28.13 text-gray-900">
+          Trabaja con nosotros
+        </p>
+      </div>
 
-export default Footer
+      <div className="flex flex-col items-center justify-center">
+        <p className="font-roboto font-normal text-base leading-28.13 text-gray-900 mb-2">
+          Seguinos en:
+        </p>
+        <div className="flex items-center justify-center">
+          <img className="w-65 h-65 mr-2" src="../../../public/images/iconosFooter/skill-icons_instagram.png" alt="Icono de instagram" />
+          <img className="w-65 h-65 mr-2" src="../../../public/images/iconosFooter/simple-icons_x.png" alt="Icono de x" />
+          <img className="w-65 h-65" src="../../../public/images/iconosFooter/devicon_facebook.png" alt="Icono de facebook" />
+        </div>
+      </div>
+
+      <div className="flex items-center justify-end">
+        <p className="font-roboto font-normal text-base leading-28.13 text-black text-24">
+          Cuenta administrador
+        </p>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
