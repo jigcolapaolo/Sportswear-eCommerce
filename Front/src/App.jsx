01e5438b57e0d95a5c2ef9c8af7f096a99f0c138
Juan <Jinbyos@Gmail.com>
2024-04-17 18:58:33 -0300

    Agrego divisor al footer. Agrego componente Hero con imagenes para el carrusel

--- Front/src/App.jsx
+++ Front/src/App.jsx
@@ -2,11 +2,13 @@ import Articulo from "./components/Articulo/Articulo";
 import { CategoryGrid } from "./components/CategoryGrid/CategoryGrid";
 import NavBar from "./components/NavBar/NavBar";
 import Footer from "./components/Footer/Footer.jsx";
+import Hero from "./components/Hero/Hero.jsx";
 
 function App() {
   return (
     <main className='bg-[#212121]'>
       <NavBar />
+      <Hero />
       <CategoryGrid />
       <Articulo />
       <Footer/>
---
+++ Front/src/components/Hero/Hero.jsx
@@ -0,0 +1,54 @@
+import React, { useRef } from 'react';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css'; // Asegúrate de importar los estilos CSS
+
+const Hero = () => {
+  const carouselRef = useRef(null);
+
+  const handleOnSlideChange = (index) => {
+    if (index === 2) {
+      setTimeout(() => {
+        carouselRef.current && carouselRef.current.next();
+      }, 100);
+    }
+  };
+
+  return (
+    <div className="pt-[62px]">
+      <Carousel
+        ref={carouselRef}
+        showArrows={true}
+        infiniteLoop={true}
+        showThumbs={false}
+        showStatus={false}
+        autoPlay={true}
+        interval={5000}
+        onChange={handleOnSlideChange}
+      >
+        <div>
+          <img
+            src="../images/imgHero/Banner1.png"
+            alt="Banner 1"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="../images/imgHero/Banner2.png"
+            alt="Banner 2"
+            className="h-full w-full object-cover"
+          />
+        </div>
+        <div>
+          <img
+            src="../images/imgHero/Banner3.png"
+            alt="Banner 3"
+            className="h-full w-full object-cover"
+          />
+        </div>
+      </Carousel>
+    </div>
+  );
+};
+
+export default Hero;
