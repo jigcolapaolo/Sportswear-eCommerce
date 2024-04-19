decbdb89979bf1dd1ce303b479bb208a3f1d05c2
Juan <Jinbyos@Gmail.com>
2024-04-19 19:10:40 -0300

    Cambio imagen del boton mobile

--- Front/src/components/NavBar/NavBar.jsx
+++ Front/src/components/NavBar/NavBar.jsx
@@ -53,30 +53,8 @@ const NavBar = () => {
                 onClick={() => setIsMobileMenuOpen(prevState => !prevState)}
               >
                 <span className="sr-only">Open main menu</span>
-                {/* <svg
-                  className={isMobileMenuOpen ? 'hidden h-6 w-6' : 'block h-6 w-6'}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                  />
-                </svg> */}
-                <svg
-                  className={isMobileMenuOpen ? 'block h-6 w-6' : 'hidden h-6 w-6'}
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
-                </svg>
+                <img src="../../../public/images/iconos/arrow.png" alt="Icono de búsqueda"
+                  className={`w-6 h-6 hover:brightness-200 ${isMobileMenuOpen ? 'rotate-0' : '-rotate-180'} transition duration-2000`} />
               </button>
             </div>
           </div>
