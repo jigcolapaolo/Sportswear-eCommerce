e41e8dd3dedc18f5f90d1b78adb827c385d382cc
Miguel Salazar <yobebe@hotmail.com.ar>
2024-04-20 19:53:55 -0300

    Aregue el icono del boton pero tengo problemas con los estilos ya que no tengo mucho conocimiento de tailwind

--- Front/src/components/Filtros/Filtros.jsx
+++ Front/src/components/Filtros/Filtros.jsx
@@ -10,9 +10,11 @@ const Filtros = () => {
     <div>
       <div className="pt-16 w-full h-57 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-700">
         <button className="w-30 h-29 top-5.35 left-4.39 border-1.5px border-gray-900 bg-gray-900">
+          <img src="../../../public/images/botones/boton-desplegable.png" alt="boton para desplegar la barra de filtros" className="w-30.83px h-29.29px t-5.35px l-4.58px b-1.5px" />
           {" "}
           Filtrar{" "}
         </button>
+
 
         <p className="font-roboto font-medium text-20 text-center text-gray-900 leading-23.44">
           Ordenar por:
