24ada24f0095fb9477ee161bc15257144561c9e6
Juan <Jinbyos@Gmail.com>
2024-04-20 23:10:42 -0300

    Modifico estilos de Filtro

--- Front/src/Catalogo.jsx
+++ Front/src/Catalogo.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ArticuloCatalogo from './components/ArticuloCatalogo/ArticuloCatalogo.jsx';
 import Footer from "./components/Footer/Footer.jsx";
 import { useLocation } from 'react-router-dom';
+import Filtros from "./components/Filtros/Filtros.jsx";
 
 function Catalogo({ agregarAlCarrito }) {
 
@@ -13,6 +14,7 @@ function Catalogo({ agregarAlCarrito }) {
 
     return (
         <main className='bg-[#212121]'>
+            <Filtros />
             <h1 className='text-5xl text-white ml-16 pt-20 pb-12 cursor-default'>Catálogo</h1>
             <ArticuloCatalogo categoryName={categoryName} searchValue={searchValue} agregarAlCarrito={agregarAlCarrito} />
             <Footer />
--- Front/src/components/Filtros/Filtros.jsx
+++ Front/src/components/Filtros/Filtros.jsx
@@ -7,48 +7,42 @@ const Filtros = () => {
   };
 
   return (
-    <div>
-      <div className="pt-16 w-full h-57 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-700">
-        <button className="w-30 h-29 top-5.35 left-4.39 border-1.5px border-gray-900 bg-gray-900">
-          <img src="../../../public/images/botones/boton-desplegable.png" alt="boton para desplegar la barra de filtros" className="w-30.83px h-29.29px t-5.35px l-4.58px b-1.5px" />
-          {" "}
-          Filtrar{" "}
+    <div className="pt-16 ">
+      <div className="fixed z-20 flex justify-around items-center w-full h-14 bg-gradient-to-r from-[#ecac30] via-yellow-500 to-[#ecac30]">
+        <button className="w-30 h-10 hover:bg-yellow-400 rounded">
+          <img src="../../../public/images/botones/boton-desplegable.png" alt="boton para desplegar la barra de filtros" className="" />
         </button>
 
 
-        <p className="font-roboto font-medium text-20 text-center text-gray-900 leading-23.44">
-          Ordenar por:
-        </p>
 
-        <div className="flex justify-end">
+        <div className="flex justify-evenly items-center gap-2">
+          <p className="font-extrabold text-2xl pr-6">
+            Ordenar por:
+          </p>
           <button
-            className={`font-roboto font-normal text-16 text-center text-gray-900 leading-18.75 ${
-              filtroSeleccionado === "masRelevantes" ? "bg-gray-400" : ""
-            }`}
+            className={`p-2 rounded border-2 border-yellow-700 text-16 text-center text-gray-900 hover:bg-yellow-400 ${filtroSeleccionado === "masRelevantes" ? "bg-yellow-400" : ""
+              }`}
             onClick={() => handleFiltroClick("masRelevantes")}
           >
             Ordenar A - Z
           </button>
           <button
-            className={`font-roboto font-normal text-16 text-center text-gray-900 leading-18.75 ${
-              filtroSeleccionado === "precioDescendente" ? "bg-gray-400" : ""
-            }`}
+            className={`p-2 rounded border-2 border-yellow-700 text-16 text-center text-gray-900 hover:bg-yellow-400 ${filtroSeleccionado === "precioDescendente" ? "bg-yellow-400" : ""
+              }`}
             onClick={() => handleFiltroClick("precioDescendente")}
           >
             Ordenar Z - A
           </button>
           <button
-            className={`font-roboto font-normal text-16 text-center text-gray-900 leading-18.75 ${
-              filtroSeleccionado === "precioAscendente" ? "bg-gray-400" : ""
-            }`}
+            className={`p-2 rounded border-2 border-yellow-700 text-16 text-center text-gray-900 hover:bg-yellow-400 ${filtroSeleccionado === "precioAscendente" ? "bg-yellow-400" : ""
+              }`}
             onClick={() => handleFiltroClick("precioAscendente")}
           >
             Mayor precio
           </button>
           <button
-            className={`font-roboto font-normal text-16 text-center text-gray-900 leading-18.75 ${
-              filtroSeleccionado === "masVendidos" ? "bg-gray-400" : ""
-            }`}
+            className={`p-2 rounded border-2 border-yellow-700 text-16 text-center text-gray-900 hover:bg-yellow-400 ${filtroSeleccionado === "masVendidos" ? "bg-yellow-400" : ""
+              }`}
             onClick={() => handleFiltroClick("masVendidos")}
           >
             Menor precio
