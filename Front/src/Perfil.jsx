331d1f41c3ef3ecc33095041fd2e06d1c814f979
Miguel Salazar <yobebe@hotmail.com.ar>
2024-04-24 16:49:20 -0300

    Arme un poco la estrutura del perfil pero quedaron problemas con los estilos. tampoco pude poner las imagenes de fondo ya que en el figma no me dejaba seleccionar esa parte y no podia acceder a las propiedades que estaban indicadas para esa parte

--- Front/src/Perfil.jsx
+++ Front/src/Perfil.jsx
@@ -2,122 +2,112 @@ import { useState } from "react";
 import LoginModal from "./components/LoginModal/LoginModal";
 
 function Perfil() {
-  const [isOpen, setIsOpen] = useState(false);
+  const [botonVisible, setBotonVisible] = useState(false);
 
-  const bottonDesplegable = () => {
-    setIsOpen(!isOpen);
-  };
+  function botonDesplegable() {
+    setBotonVisible(!botonVisible);
+  }
 
   return (
     <main className="bg-[#212121]">
-      <h1 className="absolute top-143 left-628 w-204 h-50 gap-0 opacity-0 font-rubik-mono-one text-40 font-normal leading-49.52 text-center bg-white">
+      <h1 className="style-title">
         Perfil
       </h1>
       <LoginModal />
 
-      <div className='absolute top-219 left-402 w-636 h-646 gap-0 border-t border-l border-b border-black border-opacity-10 border-solid rounded-tl-10 opacity-0"'>
-        <label> Nombre y apellido </label>
-        <button onClick={bottonDesplegable} className="">
-          Seleccionar un nombre
-        </button>
-        {isOpen && (
-          <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg">
-            <ul>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 1
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 2
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 3
-              </li>
-            </ul>
-          </div>
-        )}
-        
-        <label> Fecha de nacimiento </label>
-        <button onClick={bottonDesplegable} className="">
-          Seleccionar una fecha
-        </button>
-        {isOpen && (
-          <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg">
-            <ul>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 1
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 2
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 3
-              </li>
-            </ul>
-          </div>
-        )}
+      <div className="style-content">
+        <div className="dropDown">
+          <button
+            onClick={botonDesplegable} 
+            className="w-634 h-91 top-59 left-2 border border-t-1 border-b-0 border-l-1 border-r-0 border-yellow-500"
+          >
+            NOMBRE Y APELLIDO
+          </button>
+          {botonVisible && (
+            <div className="nextElementS">
+              <ul>
+                <li> Opcion 1 </li>
+                <li> Opcion 2 </li>
+                <li> Opcion 3 </li>
+              </ul>
+            </div>
+          )}
+        </div>
 
-        <label> Vivo en </label>
-        <button onClick={bottonDesplegable} className="">
-          Seleccionar una dirección
-        </button>
-        {isOpen && (
-          <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg">
-            <ul>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 1
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 2
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 3
-              </li>
-            </ul>
-          </div>
-        )}
+        <div className="dropDown">
+          <button
+            onClick={botonDesplegable} 
+            className="w-634 h-91 top-59 left-2 border border-t-1 border-b-0 border-l-1 border-r-0 border-yellow-500"
+          >
+            FECHA DE NACIMIENTO
+          </button>
+          {botonVisible && (
+            <div className="nextElementS">
+              <ul>
+                <li> Opcion 1 </li>
+                <li> Opcion 2 </li>
+                <li> Opcion 3 </li>
+              </ul>
+            </div>
+          )}
+        </div> 
 
-        <label> DNI </label>
-        <button onClick={bottonDesplegable} className="">
-          Seleccionar un documento
-        </button>
-        {isOpen && (
-          <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg">
-            <ul>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 1
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 2
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 3
-              </li>
-            </ul>
-          </div>
-        )}
+        <div className="dropDown">
+          <button
+            onClick={botonDesplegable} 
+            className="w-634 h-91 top-59 left-2 border border-t-1 border-b-0 border-l-1 border-r-0 border-yellow-500"
+          >
+            VIVO EN:
+          </button>
+          {botonVisible && (
+            <div className="nextElementS">
+              <ul>
+                <li> Opcion 1 </li>
+                <li> Opcion 2 </li>
+                <li> Opcion 3 </li>
+              </ul>
+            </div>
+          )}
+        </div>
 
-        <label> Tarjeta </label>
-        <button onClick={bottonDesplegable} className="">
-          Seleccionar una tarjeta
-        </button>
-        {isOpen && (
-          <div className="absolute mt-2 w-48 bg-white rounded-md shadow-lg">
-            <ul>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 1
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 2
-              </li>
-              <li className="px-4 py-2 text-gray-800 hover:bg-gray-200 cursor-pointer">
-                Option 3
-              </li>
-            </ul>
-          </div>
-        )}
+        <div className="dropDown">
+          <button
+            onClick={botonDesplegable} 
+            className="w-634 h-91 top-59 left-2 border border-t-1 border-b-0 border-l-1 border-r-0 border-yellow-500"
+          >
+            DNI:
+          </button>
+          {botonVisible && (
+            <div className="nextElementS">
+              <ul>
+                <li> Opcion 1 </li>
+                <li> Opcion 2 </li>
+                <li> Opcion 3 </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <div className="dropDown">
+          <button
+            onClick={botonDesplegable} 
+            className="w-634 h-91 top-59 left-2 border border-t-1 border-b-0 border-l-1 border-r-0 border-yellow-500"
+          >
+            TARJETA
+          </button>
+          {botonVisible && (
+            <div className="nextElementS">
+              <ul>
+                <li> Opcion 1 </li>
+                <li> Opcion 2 </li>
+                <li> Opcion 3 </li>
+              </ul>
+            </div>
+          )}
+        </div>
+
+        <button className="w-539 h-66 rounded-lg bg-orange-400 shadow-xl hover:shadow-none transition-shadow duration-500 ease-in-out"> Guardar datos </button>
       </div>
-
-      <button className="custom-style"> Guardar datos </button>
     </main>
   );
 }
